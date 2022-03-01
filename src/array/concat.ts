dbc2107a2774cd076225546b0b25614c7d7c71f7
habil99 <abiyevhabil3@gmail.com>
2022-03-02 01:49:14 +0400

    Update concat.ts

--- src/array/concat.ts
+++ src/array/concat.ts
@@ -1,14 +1,17 @@
 let result = [];
 
-export function concat<T, P>(array: T[], ...args: any[]): T[] {
-  result = [...array]
+export function concat<T>(array: T[], ...args: any): T[] {
+  result = [...array];
+
   for (let i = 0; i < args.length; i++) {
     const arg = args[i];
 
     if (typeof arg !== "object") {
       result.push(arg);
     } else {
-      concat(result, arg[0]);
+      for (const concatElement of arg) {
+        concat(result, concatElement)
+      }
     }
   }
 
--- src/lodash-lib.ts
+++ src/lodash-lib.ts
@@ -13,4 +13,4 @@ const _ = { chunk, compact, concat }
 
 // console.log(_.chunk([1, 2, 3, 4, 5, 6, 7], 2))
 // console.log(compact([0, 1, false, 2, '', 3]))
-console.log(_.concat([1], 2, "c", [4], [[3]]))
+console.log(_.concat([1], 2, [4], [[3]]))
